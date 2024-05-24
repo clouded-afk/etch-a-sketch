403fd385482285cb683d68272b54fe99d54ffecb
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-05-24 14:37:36 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Create 16x16 using JavaScript, use DOM manipulation - DO NOT add them to the html file directly the only div in the html file should be for the container to hold the grid
+    // use Flexbox to makes divs appear as a grid, note: be careful with with borders and margins, they can adjust the size of the squares
+// use a hover effect on the grid so the squares change color when the mouse passes over them and ends when the mouse leaves a square
+// Color can be changed by adding a new class to the div, or change the divs background color us javaScript
+// Add a button that will send the user a popup asking for the number of sqaures for each side of the grid
+    // once entered existing grid should be removed and new grid should be generated on the same total space as the previous grid
+    // User input should be capped at 100, any more and it can cause issues
+    // Function needs to run after button click
+
+// extra Features
+    // random colors when passing over square in the grid
+    // Progressive darkening effect, squares get darker by 10% whith each pass over the same square, being the darkest it can be after 10 passes
+        // CSS opacity would be used here
